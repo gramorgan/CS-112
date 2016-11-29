9ae966d0779984c1224881c3e5f8722e82ef6ba5
Ankur Goswami <agoswam3@ucsc.edu>
2016-11-28 21:26:15 -0800

    Update functions.pl

--- functions.pl
+++ functions.pl
@@ -6,12 +6,39 @@
 
 :- initialization(main).
 
+
 /* Degrees, minutes to radians */
-dtor(d) :-
-   write(d),
-   nl.
+
+
+dtor(D, M, R) :-
+   Pi is pi,
+   R is (M / 60 + D) * Pi / 180.
+
+
+mathfns( X, List ) :-
+   S is sin( X ),
+   C is cos( X ),
+   Q is sqrt( X ),
+   List = [S, C, Q].
+
+constants( List ) :-
+   Pi is pi,
+   E is e,
+   Epsilon is epsilon,
+   List = [Pi, E, Epsilon].
+
+sincos( X, Y ) :-
+   Y is sin( X ) ** 2 + cos( X ) ** 2.
+
+haversine_radians( Lat1, Lon1, Lat2, Lon2, Distance ) :-
+   Dlon is Lon2 - Lon1,
+   Dlat is Lat2 - Lat1,
+   A is sin( Dlat / 2 ) ** 2
+      + cos( Lat1 ) * cos( Lat2 ) * sin( Dlon / 2 ) ** 2,
+   Dist is 2 * atan2( sqrt( A ), sqrt( 1 - A )),
+   Distance is Dist * 3961.
 
 main :-
-   dtor('60'),
+   [database],
+   get_to(lax,sjc),
    halt.
-
